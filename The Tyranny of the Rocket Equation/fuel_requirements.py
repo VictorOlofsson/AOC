13c57d5612478225232e23372f2ce372c1e8c77a
VictorOlofsson <victor__o@hotmail.com>
2019-12-03 12:46:57 +0100

    Added code for part2

--- The Tyranny of the Rocket Equation/fuel_requirements.py
+++ The Tyranny of the Rocket Equation/fuel_requirements.py
@@ -16,6 +16,25 @@ def calc_req(mass_list: str) -> int:
     # return the sum of fuel
     return sum_fuel
 
+def calc_true(mass_list: str) -> int:
+    sum_fuel = 0
+    for mass in mass_list:
+        fuel = (int(mass) // 3) - 2
+        if (fuel > 0):
+            sum_fuel += fuel
+            fuel_added = fuel
+            #print("Fuel added: ", fuel_added)
+            while (fuel_added>0):
+                fuel_added = (fuel_added // 3) - 2
+                if (fuel_added < 0):
+                    fuel_added = 0
+                #print("Fuel added: ", fuel_added)
+                
+                sum_fuel += fuel_added
+    # return the sum of fuel
+    return sum_fuel
+
+
 def main():
     # File to read
     file = "mass.txt"
@@ -23,6 +42,7 @@ def main():
 
     # Print sum of fuel
     print(calc_req(list_from_file))
+    print(calc_true(list_from_file))
 
 
 if __name__== "__main__":
