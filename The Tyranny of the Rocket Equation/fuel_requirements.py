934a05619eac5b6d6717313275f70ff1606cc4ec
VictorOlofsson <victor__o@hotmail.com>
2019-12-03 10:20:45 +0100

    New repo

---
+++ The Tyranny of the Rocket Equation/fuel_requirements.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+def read_file(file: str) -> []:
+    f = open(file, "r")
+    list = f.read().splitlines()
+    f.close()
+    return list
+
+def calc_req(mass_list: str) -> int:
+    sum_fuel = 0
+
+    for mass in mass_list:
+        (int(mass) // 3)    
+    
+
+def main():
+    file = "mass.txt"
+    list_from_file = read_file(file)
+    print(list_from_file)
+
+if __name__== "__main__":
+    main() 
