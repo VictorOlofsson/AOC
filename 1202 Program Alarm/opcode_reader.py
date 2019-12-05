7b393ecd158ca00b7625d25c700637422df506c2
VictorOlofsson <victor__o@hotmail.com>
2019-12-05 08:13:46 +0100

    Updated

--- 1202 Program Alarm/opcode_reader.py
+++ 1202 Program Alarm/opcode_reader.py
@@ -39,33 +39,35 @@ def read_four(list: list) -> []:
     return list
 
 def find_noun_verb(list: list, file: str) -> int:
-    # To find correct answer part 2
-    for i in range(0, len(list)):
-        list[1] = i
-        for y in range(0, len(list)):
-            list[2] = y
-            read_four(list)
+    list_cpy = list.copy()
+    
+    for i in range(0, len(list_cpy)):
+        list_cpy[1] = i
+        for y in range(0, len(list_cpy)):
+            list_cpy[2] = y
+            read_four(list_cpy)
 
-            if(list[0] == 19690720):
-                return list[1], list[2]
+            if(list_cpy[0] == 19690720):
+                return list_cpy[1], list_cpy[2]
             else:
-                list = read_file(file)
-                list[1] = i    
+                list_cpy = list.copy()
+                list_cpy[1] = i    
 
 def main():
     # File to read
     file = "opcode.txt"
     list_from_file = read_file(file)
     # To find the correct answer part 1
-    #list_from_file[1] = 12
-    #list_from_file[2] = 2
-    #print("Correct value is:", read_four(list_from_file)[0])
+    list_from_file[1] = 12
+    list_from_file[2] = 2
+    print("Correct value part1 is:", read_four(list_from_file)[0])
 
-    # To find correct answer part 2
+    # To find the correct answer part 2
+    list_from_file = read_file(file)
     noun, verb = find_noun_verb(list_from_file,file)
     print("Noun:", noun)
     print("Verb:", verb)
-    print("Correct value is:", 100 * noun + verb)
+    print("Correct value part2 is:", 100 * noun + verb)
 
 
 
