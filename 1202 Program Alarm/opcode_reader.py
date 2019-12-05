ea1cbb17853d2656545050465819fbb6690f7f9e
VictorOlofsson <victor__o@hotmail.com>
2019-12-05 08:03:56 +0100

    Part 2

--- 1202 Program Alarm/opcode_reader.py
+++ 1202 Program Alarm/opcode_reader.py
@@ -32,16 +32,45 @@ def read_four(list: list) -> []:
             list[index3] = list[index1] * list[index2]
 
         else:
+            print(list)
             print("Unknown opcode, exiting")
             exit()
         i += 4
     return list
 
+def find_noun_verb(list: list, file: str) -> int:
+    # To find correct answer part 2
+    for i in range(0, len(list)):
+        list[1] = i
+        for y in range(0, len(list)):
+            list[2] = y
+            read_four(list)
+
+            if(list[0] == 19690720):
+                return list[1], list[2]
+            else:
+                list = read_file(file)
+                list[1] = i    
+
 def main():
     # File to read
     file = "opcode.txt"
     list_from_file = read_file(file)
-    print("Correct value is: ", read_four(list_from_file)[0])
+    # To find the correct answer part 1
+    #list_from_file[1] = 12
+    #list_from_file[2] = 2
+    #print("Correct value is:", read_four(list_from_file)[0])
+
+    # To find correct answer part 2
+    noun, verb = find_noun_verb(list_from_file,file)
+    print("Noun:", noun)
+    print("Verb:", verb)
+    print("Correct value is:", 100 * noun + verb)
+
+
+
+    
+    
 
 
 
